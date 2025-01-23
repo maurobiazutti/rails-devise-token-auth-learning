bb34504c62915c81908948f156a602e993404fca
mauro <mauro_biazutti@hotmail.com>
2025-01-23 10:15:43 -0300

    "Refactor: Update routes and add text file"

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ require "api_constraints"
 Rails.application.routes.draw do
   # root "posts#index"
   namespace :api, defaults: { format: :json } do
-    mount_devise_token_auth_for 'User', at: 'auth'
-    
+    mount_devise_token_auth_for "User", at: "auth"
+
     scope module: :v1,
       constraints: ApiConstraints.new(version: 1, default: false) do
       resources :articles
