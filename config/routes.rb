d3d82fa182daa3ff886cf45c70563eb7681902cd
mauro <mauro_biazutti@hotmail.com>
2025-01-22 22:31:11 -0300

    adding versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
+require "api_constraints"
+
 Rails.application.routes.draw do
-  resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: false) do
+      resources :articles
+    end
+
+    scope module: :v2,
+      constraints: ApiConstraints.new(version: 2, default: true) do
+      resources :articles
+    end
+  end
 end
+
+# blog/api/v1/articles: default = false version = 1
+# blog/api/v2/articles: default = true version = 2
