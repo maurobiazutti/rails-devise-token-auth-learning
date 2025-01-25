f48dfadaf92b58c7c502c32839884f700df6db11
mauro <mauro_biazutti@hotmail.com>
2025-01-24 22:01:09 -0300

    refactor(artigos): usar current_api_user e adicionar chave estrangeira na tabela de artigos

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,8 @@ class User < ActiveRecord::Base
          :rememberable,
          :trackable,
          :validatable
+
   include DeviseTokenAuth::Concerns::User
+
+  has_many :articles, dependent: :destroy
 end
--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -69,12 +69,12 @@ DeviseTokenAuth.setup do |config|
   # By default this value is expected to be sent by the client so that the API knows where 
   # to redirect users after successful email confirmation. If this param is set, the API 
   # will redirect to this value when no value is provided by the client.
-  config.default_confirm_success_url =  "http://localhost:3000/api/articles"
+  config.default_confirm_success_url =  "http://localhost:3000/api/login"
 
   # By default this value is expected to be sent by the client so that the API knows where 
   # to redirect users after successful password resets. If this param is set, the API 
   # will redirect to this value when no value is provided by the client.
-  config.default_password_reset_url = "http://localhost:3000/api/articles"
+  config.default_password_reset_url = "http://localhost:3000/api/reset_password"
 
   # By default, old tokens are not invalidated when password is changed. Enable this option 
   # if you want to make passwords updates to logout other devices.
